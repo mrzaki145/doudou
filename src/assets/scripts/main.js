0eb42cfd761f868a3ccb543da554330921d73571
mostafareda-neop <mostafa.reda@neopplanet.com>
2024-03-29 22:45:44 +0200

    init commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,32 @@
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+const time = document.querySelector("#time");
+time.textContent = getCurrentTime();
+
+function getCurrentTime(date = new Date()) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+
+// const apiKey = "YOUR_API_KEY";
+// const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+
+// // Fetching temperature data
+// fetch(`${apiEndpoint}?appid=${apiKey}&q=${encodeURIComponent('YOUR_CITY')}&units=metric`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(`The temperature in ${data.name} is ${data.main.temp}°C`);
+//   })
+//   .catch(error => console.error(error))
